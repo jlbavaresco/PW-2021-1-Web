ae462d65e321299c21ba4b39ae46adf804264620
Jorge Luis Boeira Bavaresco <jlbavaresco@gmail.com>
2021-08-19 09:25:48 -0300

    Filtro ordenação e paginação.

--- src/main/java/br/edu/ifsul/dao/CidadeDAO.java
+++ src/main/java/br/edu/ifsul/dao/CidadeDAO.java
@@ -1,5 +1,6 @@
 package br.edu.ifsul.dao;
 
+import br.edu.ifsul.converters.ConverterOrdem;
 import br.edu.ifsul.modelo.Cidade;
 import java.io.Serializable;
 import javax.ejb.Stateful;
@@ -16,6 +17,16 @@ public class CidadeDAO<TIPO> extends DAOGenerico<Cidade> implements Serializable
     public CidadeDAO(){
         super();
         classePersistente = Cidade.class;
+        // definição da lista de ordenações
+        listaOrdem.add(new Ordem("id","ID", "="));
+        listaOrdem.add(new Ordem("nome","Nome", "like"));
+        listaOrdem.add(new Ordem("estado.nome","Estado", "like"));
+        // definição da ordem atual
+        ordemAtual = listaOrdem.get(1); // vai pegar o segundo da lista de ordens
+        // criando uma instancia do conversor
+        converterOrdem = new ConverterOrdem();
+        // associando a lista de ordens ao conversor
+        converterOrdem.setListaOrdem(listaOrdem);        
     }
 
 }
--- src/main/java/br/edu/ifsul/dao/EstadoDAO.java
+++ src/main/java/br/edu/ifsul/dao/EstadoDAO.java
@@ -1,5 +1,6 @@
 package br.edu.ifsul.dao;
 
+import br.edu.ifsul.converters.ConverterOrdem;
 import br.edu.ifsul.modelo.Estado;
 import java.io.Serializable;
 import javax.ejb.Stateful;
@@ -16,6 +17,15 @@ public class EstadoDAO<TIPO> extends DAOGenerico<Estado> implements Serializable
     public EstadoDAO(){
         super();
         classePersistente = Estado.class;
+        // definição da lista de ordenações
+        listaOrdem.add(new Ordem("id","ID", "="));
+        listaOrdem.add(new Ordem("nome","Nome", "like"));
+        // definição da ordem atual
+        ordemAtual = listaOrdem.get(1); // vai pegar o segundo da lista de ordens
+        // criando uma instancia do conversor
+        converterOrdem = new ConverterOrdem();
+        // associando a lista de ordens ao conversor
+        converterOrdem.setListaOrdem(listaOrdem);
     }
 
 }
